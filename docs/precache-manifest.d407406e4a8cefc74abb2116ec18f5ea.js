b894adcc8e900698f702399b6b2c84384f7f5796
Filippo Del Prete <filippo.delprete@gmail.com>
2019-04-29 16:50:27 +0200

    docs folder for github pages

---
+++ docs/precache-manifest.d407406e4a8cefc74abb2116ec18f5ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fdac2d35ef682d7ccacd3a6950e08f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28ddbafea90651e998a8",
+    "url": "/static/css/main.583d9439.chunk.css"
+  },
+  {
+    "revision": "f09545b358d42bf9c016",
+    "url": "/static/js/2.ca038051.chunk.js"
+  },
+  {
+    "revision": "28ddbafea90651e998a8",
+    "url": "/static/js/main.5fa16abd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
